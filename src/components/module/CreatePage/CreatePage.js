55f60149357ade6f251c23ccb2f2804858c9a68f
alearm <alearm246@gmail.com>
2020-11-28 13:27:19 -0600

    added header component

---
+++ src/components/module/CreatePage/CreatePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../common/Header/Header.js";
+
+function CreatePage(){
+  return(
+    <div>
+      <Header />
+      <p>create page</p>
+    </div>
+  )
+}
+
+export default CreatePage;
--- src/components/module/PokemonCardsPage/PokemonCardsPage.js
+++ src/components/module/PokemonCardsPage/PokemonCardsPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import style from "./PokemonCardsPage.module.css";
 import PokemonCards from "../../common/PokemonCards/PokemonCards.js";
+import Header from "../../common/Header/Header.js";
 
 
 function PokemonCardsPage(){
   return (
     <div className={style.pokemonCardsPage}>
+      <Header />
       <PokemonCards />
     </div>
   )
